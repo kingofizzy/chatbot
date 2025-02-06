87b03eee12ab72028ef338dc3f1b15bb20887974
Hyper Network <123313247+kingofizzy@users.noreply.github.com>
2025-02-06 09:07:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,14 +9,14 @@ API_ID = "6435225"
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", "7551118575:AAHAHM8vtLD0FD2Zeld5feLXxCDRgGoSUps")
-STRING1 = getenv("STRING_SESSION", None)
+STRING1 = getenv("STRING_SESSION", "BQC86fAAff9lE8MLuow-iVQYtdvKUudi2y7cF-em1T0ip_FSXyKkaiaSoYWcN_-e5ytovWXcvyt1sCqWqL7SocByc2RHHdS7fDc6j5qAK62Sm3TD7NDlRU9rdGVP8nY0nLyMcvrAY2d-Plhby8222k6adTSK9j6epXPma2V6AivEChdzPtWrp-7S8aPUbmyJwsUHlj19CcOVoAdF0TMXnGEGGJ85gV0HYCBAflnwKHn7NOliFkHnF_rx3vEEpqQOLURvFeWfb8LEISHhLMzO5-a_CKrRn3QHUZpoo-ZqeCkAgQ2MZJmVsO49EGMjpj4ITOKUWzF1ES8tNbY3MYSDb0S1tSBTGwAAAAHV1eN-AA")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://architect04:architect04@cluster0.fylqb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-OWNER_ID = int(getenv("OWNER_ID", "7520092354"))
+OWNER_ID = int(getenv("OWNER_ID", "7882531710"))
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/yourtoofan/CHATBOT")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 SUPPORT_GRP = "THE_INCRICIBLE"
 UPDATE_CHNL = "THE_INCRICIBLE"
-OWNER_USERNAME = "LEGEND_MICKEY"
+OWNER_USERNAME = "Itz_me_Makkaru"
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", "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")
     
